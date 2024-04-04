cd7c92f18802a7068a0b5cc483bb98c1cbe5278b
Bchiapello <brunotlf@gmail.com>
2024-04-04 20:06:30 +0200

    second commit

---
+++ capstonemeta/src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+      <img src='capstonemeta\src\assets\title.jpg' alt='Logo'/>
+      <ul>
+        <li><a href='http://google.com'>Home</a></li>
+        <li><a href='http://google.com'>About</a></li>
+        <li><a href='http://google.com'>Home</a></li>
+        <li><a href='http://google.com'>Home</a></li>
+        <li><a href='http://google.com'>Home</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Nav
